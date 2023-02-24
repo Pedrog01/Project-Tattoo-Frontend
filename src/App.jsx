6ec46b8e01807d4d433241504aef605487d68599
Diogo <diogoslva0133@gmail.com>
2023-02-24 10:11:02 -0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css'
+
+import RouterApp from './Routes';
+
+const App = () => <RouterApp/>
+
+export default App;
---
+++ src/Routes/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Router,Route, Routes,} from 'react-router-dom';
+
+const RouterApp = () => {
+  return (
+    <h1>aa</h1>
+  )
+}
+
+export default RouterApp;
+
+
