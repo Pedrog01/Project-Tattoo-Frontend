c3c8b1c1506cbd09ff9af48c11ed149966945120
Diogo <diogoslva0133@gmail.com>
2023-03-07 12:31:58 -0300

    chore: fixing some mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import './index.css'
 
 import RouterApp from './Routes';
 
-const App = () => <RouterApp/>
+const App = () => <> <ToastContainer/> ,<RouterApp/> </>
 
 export default App;
--- src/Pages/signIn/index.jsx
+++ src/Pages/signIn/index.jsx
@@ -16,7 +16,7 @@ const SignIn = () => {
     try {
       const response = await connection.post("/auth/sign_in", { email, password });
        console.log(response)
-       navigate('/')
+       navigate('/posts')
        toast.success("logged in user!")
     } catch (error) {
       toast.error("Incorrect email or password!");
@@ -26,7 +26,9 @@ const SignIn = () => {
   return (
     <div className="flex items-center min-h-screen bg-zinc-900"> 
       <div class="px-8 py-6 mx-10 mt-4 text-left bg-white rounded-md shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
-        <h2 class="text-2xl font-bold text-center">Login</h2>
+      <h1 className=" flex justify-center ">
+        <p className="font-Montserrat text-md text-[#1b0096]">Login</p>
+        </h1>
         <div className="form-data">
           <div class="mt-4">
             <form onSubmit={handleSubmit}> 
@@ -56,7 +58,11 @@ const SignIn = () => {
                 </label>
               </div>
               <div>
-              <button class="w-full px-6 py-2 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-800">SignIn</button>
+              <button class="w-full px-6 py-2 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-800">
+                <p className="font-Montserrat text-sm text-white">
+                  SignIn
+                </p>
+                </button>
               </div>
                <div class="mt-5 text-grey-dark">
                   Already have an account?
